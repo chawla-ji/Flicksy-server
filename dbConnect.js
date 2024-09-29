6366fb77b2f7bb5478810fb65d14c37a74ca11a2
Diya <diya@192.168.137.88>
2024-09-29 18:56:05 +0530

    remove .env

--- dbConnect.js
+++ dbConnect.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
     const mongoUri =
-        "mongodb+srv://anuj:Ya0fKBalld60gpfs@cluster0.rblpaxc.mongodb.net/?retryWrites=true&w=majority";
-
+    process.env.MONGODB_URI;
     try {
         const connect = await mongoose.connect(mongoUri, {
             useUnifiedTopology: true,
